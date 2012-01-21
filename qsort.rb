2913f633793f54426eb3dba9c00aa2af0ef00ff8
David Kasper <dkasper@gmail.com>
2012-01-21 14:44:33 -0800

    first commit

---
+++ qsort.rb
@@ -0,0 +1,18 @@
+def quicksort(l)
+  if l.length <= 1
+    return l
+  end
+  pivot = l.pop
+  less,greater = l.partition { |v| v < pivot}
+  quicksort(less) + [pivot] + quicksort(greater)
+end
+
+if __FILE__ == $0
+  a = gets.strip.split
+else
+  a = []
+  while a.length < 50
+    a << rand(1000000)
+  end
+end
+puts quicksort(a).to_s
